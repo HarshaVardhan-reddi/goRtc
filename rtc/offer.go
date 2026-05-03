9cb9e94535380391ffa7be42ac4cc1f93a2e5c5a
HarshaVardhan-reddi <harshapreddy9@gmail.com>
2026-05-03 19:04:16 +0530

    Feat: implement offer and answer creation functions for WebRTC

--- rtc/offer.go
+++ rtc/offer.go
@@ -1,1 +1,38 @@
 package rtc
+
+import (
+	"log"
+
+	"github.com/pion/webrtc/v4"
+)
+
+func CreateOffer(webrtcConn *webrtc.PeerConnection) (*webrtc.SessionDescription, error) {
+	sdp, err := webrtcConn.CreateOffer(nil)
+	if(err != nil){
+		return nil, err
+	}
+	webrtcConn.SetLocalDescription(sdp)
+	return &sdp,nil
+}
+
+func SetRemoteSdp(webrtcConn *webrtc.PeerConnection, remoteSdp string, sdpType webrtc.SDPType) error {
+	desc := webrtc.SessionDescription{
+		Type: sdpType,
+		SDP: remoteSdp,
+	}
+	return webrtcConn.SetRemoteDescription(desc)
+}
+
+func CreateAnswer(webrtcConn *webrtc.PeerConnection,remoteSdp string) (*webrtc.SessionDescription, error) {
+	if err := SetRemoteSdp(webrtcConn, remoteSdp, webrtc.SDPTypeOffer); err != nil{
+		log.Println(err)
+	}
+	sdp, err := webrtcConn.CreateAnswer(nil)
+	if(err != nil){
+		return nil, err 
+	}
+	if err := webrtcConn.SetLocalDescription(sdp); err != nil{
+		log.Println(err)
+	}
+	return  &sdp, nil
+}
